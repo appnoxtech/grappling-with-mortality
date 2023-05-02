39b56890dc643f21d3fda0d25c4cbe244ca1e75c
Shudhanshu-Appnox <shudhanshu.choudhary@appnox.ai>
2023-05-02 16:11:32 +0530

    Added interfaces for the global store

--- src/interfaces/author/book.interface.ts
+++ src/interfaces/author/book.interface.ts
@@ -1,12 +1,30 @@
+import { Chapter } from "./chapter.interface";
+
 export interface book {
-    bookName: string;
-    bookImage: string;
-    description: string;
-    authorName: string;
-    authorImage: string;
-    noOfPages: number;
-    bookLink: string;
-    chapters: any;
+  id?: string,
+  bookName: string;
+  bookImage: string;
+  description: string;
+  authorName: string;
+  authorImage: string;
+  noOfPages: number;
+  bookLink: string;
+  chapters: any;
+}
+
+export interface Audio {
+  _id?: string,
+  audioLink: string,
+  chapterName: string,
+
+}
+
+export interface bookDetails extends book {
+  _id?: string,
+  audio: Array<Audio>,
+  audioId: string,
+  authorId: string,
+  chapters: Array<Chapter>,
 }
 
 // Author Reducer Interface and type
@@ -14,31 +32,65 @@ export interface book {
 // ACTIONS TYPES
 export const UPDATE_AUTHOR_BOOK_LIST = 'UPDATE_AUTHOR_BOOK_LIST';
 export const UPDATE_NEW_BOOK_DETAILS = 'UPDATE_NEW_BOOK_DETAILS';
+export const CLEAR_NEW_BOOK_DETAILS = 'CLEAR_NEW_BOOK_DETAILS';
+export const UPDATE_SELECTED_BOOK = 'UPDATE_SELECTED_BOOK';
+export const UPDATE_SELECTED_BOOK_DETAILS = 'UPDATE_SELECTED_BOOK_DETAILS';
+export const UPDATE_CHAPTERS_LIST = 'UPDATE_CHAPTERS_LIST';
+export const EDIT_NEW_BOOK = 'EDIT_NEW_BOOK';
 
-export type NewBookUpdateKey = 'bookName' | 'bookImage' | 'description' | 'authorName' | 'authorImage' | 'noOfPages' | 'bookLink';
+export type NewBookUpdateKey =
+  | 'bookName'
+  | 'bookImage'
+  | 'description'
+  | 'authorName'
+  | 'authorImage'
+  | 'noOfPages'
+  | 'bookLink';
+
 
 export interface NewBookData {
-    key: NewBookUpdateKey,
-    value: string | number
+  key: NewBookUpdateKey;
+  value: string | number;
 }
 
 export interface UpdateBookList {
-   type: 'UPDATE_AUTHOR_BOOK_LIST',
-   payload: Array<book>
-};
+  type: 'UPDATE_AUTHOR_BOOK_LIST';
+  payload: Array<book>;
+}
 
 export interface updateNewBookDetails {
-    type: 'UPDATE_NEW_BOOK_DETAILS',
-    payload: NewBookData
+  type: 'UPDATE_NEW_BOOK_DETAILS';
+  payload: NewBookData;
 }
 
 export interface clearNewBookDetails {
-    type: 'CLEAR_NEW_BOOK_DETAILS',
+  type: 'CLEAR_NEW_BOOK_DETAILS';
 }
 
 export interface updateSelectedBook {
-    type: 'UPDATE_SELECTED_BOOK',
-    payload: any
+  type: 'UPDATE_SELECTED_BOOK';
+  payload: any;
 }
 
-export type action = UpdateBookList | updateNewBookDetails | clearNewBookDetails | updateSelectedBook
+export interface updateSelectedBookDetails {
+  type: 'UPDATE_SELECTED_BOOK_DETAILS';
+  payload: any;
+}
+
+export interface updateChapterList {
+  type: 'UPDATE_CHAPTERS_LIST';
+  payload: any;
+}
+
+export interface editNewBook {
+  type: 'EDIT_NEW_BOOK'
+}
+
+export type action =
+  | UpdateBookList
+  | updateNewBookDetails
+  | clearNewBookDetails
+  | updateSelectedBook
+  | updateSelectedBookDetails
+  | updateChapterList
+  | editNewBook;
---
+++ src/interfaces/reducer/state.ts
@@ -0,0 +1,13 @@
+import { AuthStoreInterface } from "./authStore.Interface";
+import { AuthorStoreInterface } from "./authorStore.interface";
+import { ChapterStoreInterface } from "./chapter.interface";
+import { CommonStoreInterface } from "./commonStore.interface";
+import { userStore } from "./user.interface";
+
+export interface store {
+    user: userStore,
+    authDetails: AuthStoreInterface,
+    author: AuthorStoreInterface,
+    common: CommonStoreInterface,
+    chapter: ChapterStoreInterface
+}
